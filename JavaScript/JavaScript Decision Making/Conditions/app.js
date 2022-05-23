8c18eb68d9a551761a84e86b65d49bd93cd57c06
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-05-23 16:47:38 +0530

    Completed elseIf Conditions JS

--- JavaScript/JavaScript Decision Making/Conditions/app.js
+++ JavaScript/JavaScript Decision Making/Conditions/app.js
@@ -1,3 +1,17 @@
 if(1+2===3){
     console.log("condition is true");
 }
+
+// 0-5 free 
+// 5-10 $10
+// 10-65 $20
+// 65+ $10
+if(age<=5){
+    console.log("Your r a baby, Free");
+}else if(age<=10){
+    console.log("Your r a child, $10");
+}else if(age<=65){
+    console.log("Your r a adult, $20");
+}else{
+    console.log("Your r a senior, $10");
+}
