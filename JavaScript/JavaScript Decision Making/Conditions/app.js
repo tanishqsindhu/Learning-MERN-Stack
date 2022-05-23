6f03f53d565a19c8749ce2a387f0d966220dcdb4
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-05-23 16:50:46 +0530

    Completed else Condition JS

--- JavaScript/JavaScript Decision Making/Conditions/app.js
+++ JavaScript/JavaScript Decision Making/Conditions/app.js
@@ -1,7 +1,7 @@
 if(1+2===3){
     console.log("condition is true");
 }
-
+const age=5;
 // 0-5 free 
 // 5-10 $10
 // 10-65 $20
@@ -14,4 +14,16 @@ if(age<=5){
     console.log("Your r a adult, $20");
 }else{
     console.log("Your r a senior, $10");
+}
+
+const day='Mon';
+
+if(day==='Fri'){
+    console.log("Decent after work hours");
+}else if(day==='Sat'){
+    console.log("great");
+}else if(day==='Sun'){
+    console.log("good");
+}else{
+    console.log("Meh");
 }
