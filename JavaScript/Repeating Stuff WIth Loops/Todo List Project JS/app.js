9ed954a9ac22dee905192562eeec747d43345cee
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-03 18:10:53 +0530

    Completed ToDoList Project JavaScript

--- JavaScript/Repeating Stuff WIth Loops/Todo List Project JS/app.js
+++ JavaScript/Repeating Stuff WIth Loops/Todo List Project JS/app.js
@@ -1,21 +1,24 @@
 let list=[];//array for storing list.
 while(true){
-    let answer=prompt("What you want to Do?").toLowerCase();
+    let answer=prompt("What you want to Do?").toLowerCase().trim();
     if(answer==='new'){
         let task =prompt("What task do you want to add?")
         list.push(task);
+        console.log(`${task}is added to the List!!`)
     }
     if(answer==='list'){
         for(let index in list){
-            console.log(`${index} : ${list[index]}`);
+            let realNo=index+1;
+            console.log(`${realNo} : ${list[index]}`);
         }
     }
     if(answer==='delete'){
         let index =parseInt(prompt("What is the index you want to delete?"))
         while(index>list.length||index<0){let index =parseInt(prompt("What is the index you want to delete?"))}
-        list=list.slice(0,index);
+        list.splice(index,1)
+        console.log(list);
     }
-    if(answer==='quit'){
+    if(answer==='quit'||answer==='q'){
         console.log("You are Free!!!!")
         break;
     }
