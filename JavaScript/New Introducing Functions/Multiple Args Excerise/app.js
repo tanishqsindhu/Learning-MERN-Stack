79c189865eeea827868283d728492d317fce4117
tanishqsindhu <hisar.champion@gmail.com>
2022-07-04 18:17:57 +0530

    Completed Multiple Args Exercise

---
+++ JavaScript/New Introducing Functions/Multiple Args Excerise/app.js
@@ -0,0 +1,9 @@
+// define isSnakeEyes below:
+function isSnakeEyes(number1,number2){
+    if(number1===1&&number2===1){
+        console.log("Snake Eyes!")
+    }else{
+        console.log("Not Snake Eyes!")
+    }
+}
+isSnakeEyes(1,5);
