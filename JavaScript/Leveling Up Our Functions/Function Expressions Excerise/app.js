bbac29045f1a17d5c64ac8344e69223e0d7ff378
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-08 19:50:12 +0530

    Completed Function Expressions Exercise

---
+++ JavaScript/Leveling Up Our Functions/Function Expressions Excerise/app.js
@@ -0,0 +1,5 @@
+//NOTE: Udemy's coding exercise platform does NOT support the ** operator.  You'll need to multiply a number by itself or use the Math.pow() method.
+let square=function(number){
+    return Math.pow(number,2);
+}
+square(16);
