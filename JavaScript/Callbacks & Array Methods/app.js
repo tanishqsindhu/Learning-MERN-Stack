1a88e46948a6bb7bf72f4a50156d0a302777b957
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-16 15:40:31 +0530

    Completed Arrow Function Implicit Returns

---
+++ JavaScript/Callbacks & Array Methods/app.js
@@ -0,0 +1,9 @@
+//Arrow Function Implicit Returns
+const rollDie = ()=>(
+    Math.floor(Math.random()*6)+1
+)
+const add=(a,b)=>(
+    a+b
+)
+const add3 =(a,b,c)=>a+b+c
+//only one value must be present for implicit Functions
