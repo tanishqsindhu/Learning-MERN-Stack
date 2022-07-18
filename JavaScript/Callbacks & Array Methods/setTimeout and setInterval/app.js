f575889bb50ba192014d9b1094e5dbc8f86db6ca
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-19 00:29:24 +0530

    Completed setTimeout and setInterval

---
+++ JavaScript/Callbacks & Array Methods/setTimeout and setInterval/app.js
@@ -0,0 +1,9 @@
+console.log("Hello!")
+setTimeout=(()=>{
+    console.log(".....are you still there!!!")
+},2000)
+console.log("GoodBye!!!")
+
+const id=setInterval=(()=>{
+    console.log(Math.random())
+},3000)
