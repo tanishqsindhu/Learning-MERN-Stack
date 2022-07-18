faa533454bbcb92c12a9a9e66a4d50778371112f
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-19 00:36:07 +0530

    Completed The filter Method

--- JavaScript/Callbacks & Array Methods/app.js
+++ JavaScript/Callbacks & Array Methods/app.js
@@ -7,3 +7,7 @@ const add=(a,b)=>(
 )
 const add3 =(a,b,c)=>a+b+c
 //only one value must be present for implicit Functions
+
+//The filter Method
+const numbers=[1,2,3,4,5,6,7,8,9,10]
+const odd=numbers.filter(n=>{return n%2==1})
