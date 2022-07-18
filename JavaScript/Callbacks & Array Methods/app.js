24de5488ae9e19985f2cf254e7b1de141f76bbf7
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-19 00:51:03 +0530

    Completed Some & Every

--- JavaScript/Callbacks & Array Methods/app.js
+++ JavaScript/Callbacks & Array Methods/app.js
@@ -11,3 +11,10 @@ const add3 =(a,b,c)=>a+b+c
 //The filter Method
 const numbers=[1,2,3,4,5,6,7,8,9,10]
 const odd=numbers.filter(n=>{return n%2==1})
+
+//Every Method
+const marks=[80,76,74,80,90,91,89]
+marks.every(n=>n>=75);
+
+//Some Method
+marks.some(n=>n>=75);
