6a429c0e84f9957274880637cf0e08fb4117fa7a
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-07-31 18:04:41 +0530

    Manipulating Attributes Practice Completed

---
+++ JavaScript/Introduction The World Of The DOM/Manipulating Attributes Practice/app.js
@@ -0,0 +1,3 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+document.querySelector('img').setAttribute('src','https://devsprouthosting.com/images/chicken.jpg');
+document.querySelector('img').alt = 'chicken';
