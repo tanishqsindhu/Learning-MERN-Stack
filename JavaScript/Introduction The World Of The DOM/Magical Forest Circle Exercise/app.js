388114f9b033cd7bd77a5f8378cf05bed3b7037b
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-01 19:22:40 +0530

    Magical Forest Circle Exercise Completed

---
+++ JavaScript/Introduction The World Of The DOM/Magical Forest Circle Exercise/app.js
@@ -0,0 +1,5 @@
+const container =document.querySelector('#container');
+container.style.textAlign='center';
+const image=document.querySelector('img');
+image.style.width='150px';
+image.style.borderRadius='50%';
