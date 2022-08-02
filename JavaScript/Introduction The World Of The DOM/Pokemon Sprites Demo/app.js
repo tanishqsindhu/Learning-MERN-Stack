5aa2688a1364bf7918ee3908ebe9f316d5c130fc
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-03 01:52:32 +0530

    Fixed somethings

--- JavaScript/Introduction The World Of The DOM/Pokemon Sprites Demo/app.js
+++ JavaScript/Introduction The World Of The DOM/Pokemon Sprites Demo/app.js
@@ -1,8 +1,9 @@
 //https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 const container=document.querySelector('#container');
 const baseURL='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
-for(let i=1;i<=150;i++){
+for(let i=1;i<151;i++){
     const pokemon=document.createElement('div');
+    pokemon.classList.add('pokemon');
     const label =document.createElement('span');
     label.innerText=`#${i}`;
     const newImg =document.createElement('img');
