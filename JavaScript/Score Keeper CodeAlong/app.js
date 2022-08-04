d1b2a76d0b6600ae57f1eb57ab0e7231ad24f70c
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-04 23:55:33 +0530

    Part 1 Completed

--- JavaScript/Score Keeper CodeAlong/app.js
+++ JavaScript/Score Keeper CodeAlong/app.js
@@ -1,7 +1,26 @@
-const p1Button=document.querySelector('#player1button');
-const p2Button =document.querySelector('#player2button')
+const p1Button=document.querySelector('#p1button');
+const p2Button =document.querySelector('#p2button');
 let p1Score=0,p2Score=0;
-
+const p1Display =document.querySelector('#p1Display')
+const p2Display =document.querySelector('#p2Display')
+let winningScore=5;
+let isGameOver= false;
 p1Button.addEventListener('click',()=>{
-    p1Score++;
+    if(!isGameOver){
+        p1Score++;
+        if(p1Score===winningScore){
+            isGameOver=true;
+        }
+        p1Display.textContent=p1Score;
+    }
+
+})
+p2Button.addEventListener('click',()=>{
+    if(!isGameOver){
+        p2Score++;
+        if(p2Score===winningScore){
+            isGameOver=true;
+        }
+        p2Display.textContent=p2Score;
+    }
 })
