ddbc4b9b00bb571b3d7228e965d108e68ab5b0da
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-04 15:24:36 +0530

    Click Event Exercise Completed

--- JavaScript/The Missing Piece DOM Events/Click Events Exercise/app.js
+++ JavaScript/The Missing Piece DOM Events/Click Events Exercise/app.js
@@ -1,25 +1,4 @@
-const btn =document.querySelector('#v2');
-
-btn.onclick=function(){
-    console.log("You Clicked Me!!");
-    console.log("I HoPE IT WORKED!");
-}
-function scream(){
-    console.log("AAAAAHHHHH");
-    console.log("STOP TOUCHING ME!");
-}
-const btn3=document.querySelector('#v3')
-btn3.addEventListener('click',function(){
-    alert("CLICKED!")
-})
-function twist(){
-    console.log("twist");
-}
-function Shout(){
-    console.log("Shout!")
-}
-const tasButton =document.querySelector('#tas');
-tasButton.addEventListener('click', twist, {once:true})
-tasButton.addEventListener('click', Shout)
-
-btn.onmouseenter = scream;
+const helloButton=document.querySelector('#hello');
+const goodbyeButton=document.querySelector('#goodbye')
+helloButton.addEventListener('click',()=>{console.log("hello")});
+goodbyeButton.addEventListener('click',()=>{console.log("goodbye")});
