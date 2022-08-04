d2b060d09902d5613a5ca93be968f0a482b26d65
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-05 00:41:15 +0530

    Part 3 Completed

--- JavaScript/Score Keeper CodeAlong/app.js
+++ JavaScript/Score Keeper CodeAlong/app.js
@@ -15,6 +15,10 @@ p1Button.addEventListener('click',()=>{
         p1Score++;
         if(p1Score===winningScore){
             isGameOver=true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled=true;
+            p2Button.disabled=true;
         }
         p1Display.textContent=p1Score;
     }
@@ -25,6 +29,10 @@ p2Button.addEventListener('click',()=>{
         p2Score++;
         if(p2Score===winningScore){
             isGameOver=true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled=true;
+            p2Button.disabled=true;
         }
         p2Display.textContent=p2Score;
     }
@@ -42,4 +50,8 @@ function reset(){
     isGameOver=false;
     p1Display.textContent=p1Score;
     p2Display.textContent=p2Score;
+    p1Display.classList.remove('has-text-success','has-text-danger');
+    p2Display.classList.remove('has-text-success','has-text-danger');
+    p1Button.disabled=false;
+    p2Button.disabled=false;
 }
