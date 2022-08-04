6779a29f641b1e216a93aa7ab1165c17df31c293
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-04 19:35:55 +0530

    Completed Input Event Practice Exercise

--- JavaScript/The Missing Piece DOM Events/Input Event Practice Exercise/app.js
+++ JavaScript/The Missing Piece DOM Events/Input Event Practice Exercise/app.js
@@ -1,5 +1,9 @@
 const h1=document.querySelector('h1');
 const input =document.querySelector('input');
 input.addEventListener('input',(e)=>{
-    h1.innerText=input.value;
+        if(!input.value){
+        h1.innerText='Enter Your Username';
+    }else{
+        h1.innerText=`Welcome, ${input.value}`;
+    }
 })
