4e5b5d141cdf3029f64d9085d45da9105df04c8f
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-04 19:59:57 +0530

    .

---
+++ JavaScript/Score Keeper CodeAlong/app.js
@@ -0,0 +1,7 @@
+const p1Button=document.querySelector('#player1button');
+const p2Button =document.querySelector('#player2button')
+let p1Score=0,p2Score=0;
+
+p1Button.addEventListener('click',()=>{
+    p1Score++;
+})
