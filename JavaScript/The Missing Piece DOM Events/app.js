0b56c6034f358f6fb9cf76248235f812218a12ab
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-04 15:07:55 +0530

    addEventListener Completed

--- JavaScript/The Missing Piece DOM Events/app.js
+++ JavaScript/The Missing Piece DOM Events/app.js
@@ -8,4 +8,18 @@ function scream(){
     console.log("AAAAAHHHHH");
     console.log("STOP TOUCHING ME!");
 }
+const btn3=document.querySelector('#v3')
+btn3.addEventListener('click',function(){
+    alert("CLICKED!")
+})
+function twist(){
+    console.log("twist");
+}
+function Shout(){
+    console.log("Shout!")
+}
+const tasButton =document.querySelector('#tas');
+tasButton.addEventListener('click', twist, {once:true})
+tasButton.addEventListener('click', Shout)
+
 btn.onmouseenter = scream;
