0896c51eb577f0583e9f09a62703f3d5c188f3c8
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-06 18:48:41 +0530

    Setting Headers With Axions Completed

--- JavaScript/AJAX and API's/Axios/app.js
+++ JavaScript/AJAX and API's/Axios/app.js
@@ -15,5 +15,26 @@ const getStarWArsPerson = async(id)=>{
         console.log("ERROR!",e);
     }
 };
-getStarWArsPerson(5);
-getStarWArsPerson(6);
+
+
+const jokes =document.querySelector('#jokes');
+const button =document.querySelector('button')
+
+const addNewJoke= async()=>{
+    const jokeText = await getDadJoke()
+    const newLi= document.createElement('li');
+    newLi.append(jokeText);
+    jokes.append(newLi)
+}
+const getDadJoke = async ()=>{
+    try {
+        const config ={headers: {Accept: 'application/json'}}
+        const res = await axios.get('https://icanhazdadjoke.com/',config)
+        return res.data.joke;
+    } catch (error) {
+        return `NO JOKES AVAILABLE! SORRY :(${error}`
+    }
+
+}
+
+button.addEventListener('click',addNewJoke)
