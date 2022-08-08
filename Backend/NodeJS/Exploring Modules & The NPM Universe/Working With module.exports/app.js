36144a489ff097f7b7b60eaba0eccbf776566903
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-08-09 01:21:08 +0530

    Working With module.exports Completed

---
+++ Backend/NodeJS/Exploring Modules & The NPM Universe/Working With module.exports/app.js
@@ -0,0 +1,4 @@
+const {PI,square} = require('./math');
+// console.log(math)
+console.log(PI)
+console.log(square(9))
---
+++ Backend/NodeJS/Exploring Modules & The NPM Universe/Working With module.exports/math.js
@@ -0,0 +1,13 @@
+// module.exports.const.add = (x,y)=>x+y;
+const PI = 3.14159;
+const square = x => x*x;
+const math={
+    PI:PI,
+    square:square
+}
+// module.exports.add=add;
+// module.exports.PI=PI;
+// module.exports.square=square;
+module.exports.math
+exports.square =square;
+exports.PI=PI;
