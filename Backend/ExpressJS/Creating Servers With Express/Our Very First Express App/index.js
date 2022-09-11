2e99235656f2603b60b7fa1a4a383411876fc5fa
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-09-11 16:19:35 +0530

    The Request & Response Objects Completed

--- Backend/ExpressJS/Creating Servers With Express/Our Very First Express App/index.js
+++ Backend/ExpressJS/Creating Servers With Express/Our Very First Express App/index.js
@@ -1,7 +1,12 @@
 const express =require("express");
 const app =express();
-app.use(()=>{
+let count=0;
+app.use((req,res)=>{
     console.log("WE GOT A NEW REQUEST!!!");
+    console.dir(req);
+    count++;
+    //res.send(`HELLO WE GOT YOUR REQUEST, THIS IS YOUR RESPONSE ${count}`)
+    res.send({color:"red"});
 })
 app.listen(3000,()=>{
     console.log("LISTENING ON PORT 3000");
