639c50163a845ae96b6bdb46fc81e0dca1a3e9ea
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-10-04 02:07:42 +0530

    Setting views directory Completed

--- Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
+++ Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-
+const path=require('path');
 app.set('view engine', 'ejs');
-
+//setting views directory
+app.set('views',path.join(__dirname,'/views'))
 app.get('/',(req,res)=>{
-    res.render('home.ejs')
+    res.render('home')
 })
 app.listen(3000,()=>{
     console.log("LISTENING ON PORT 3000")
