9f28bad57b19bbfb8e2568513a725d133cff8b1b
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-10-20 12:02:22 +0530

    Subreddit Template Demo Completed

--- Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
+++ Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
@@ -11,6 +11,10 @@ app.get('/rand',(req,res)=>{
     const num =Math.floor(Math.random()*10)+1
     res.render('random',{rand: num})
 })
+app.get('/r/:subreddit',(req,res)=>{
+    const {subreddit}=req.params;
+    res.render('subreddit',{subreddit});
+})
 app.listen(3000,()=>{
     console.log("LISTENING ON PORT 3000")
 })
