56083ee39a8969c995d81a3bc9b9de0a11e74833
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-10-20 11:55:55 +0530

    Passing Data To Templates Completed

--- Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
+++ Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
@@ -7,6 +7,10 @@ app.set('views',path.join(__dirname,'/views'))
 app.get('/',(req,res)=>{
     res.render('home')
 })
+app.get('/rand',(req,res)=>{
+    const num =Math.floor(Math.random()*10)+1
+    res.render('random',{rand: num})
+})
 app.listen(3000,()=>{
     console.log("LISTENING ON PORT 3000")
 })
