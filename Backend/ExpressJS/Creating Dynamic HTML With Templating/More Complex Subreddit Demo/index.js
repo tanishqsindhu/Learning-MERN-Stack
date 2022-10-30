e23335cf741f6afbcb5f7e45398ab84fa8adf184
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-10-30 17:58:26 +0530

    More Complex Subreddit Demo Completed

---
+++ Backend/ExpressJS/Creating Dynamic HTML With Templating/More Complex Subreddit Demo/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path=require('path');
+const redditData = require('./data.json');
+
+app.set('view engine', 'ejs');
+//setting views directory
+app.set('views',path.join(__dirname,'/views'))
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+app.get('/rand',(req,res)=>{
+    const num =Math.floor(Math.random()*10)+1
+    res.render('random',{rand: num})
+})
+app.get('/cats',(req,res)=>{
+    const cats =[
+        'Blue','Rocket','Monty','Red','Winston'
+    ]
+    res.render('cats',{cats})
+})
+app.get('/r/:subreddit',(req,res)=>{
+    const {subreddit}=req.params;
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit',{...data});
+    }else{
+        res.render('notfound',{subreddit})
+    }
+
+})
+app.listen(3000,()=>{
+    console.log("LISTENING ON PORT 3000")
+})
