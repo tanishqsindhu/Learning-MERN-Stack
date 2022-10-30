3c7fcd4e0f4f86ffeb0c5d6dbc643586a95a4873
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-10-30 19:43:45 +0530

    Serving Static Assets in Express Completed

--- Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
+++ Backend/ExpressJS/Creating Dynamic HTML With Templating/Configuring Express For EJS/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const path=require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname,'public')))
+
 app.set('view engine', 'ejs');
 //setting views directory
 app.set('views',path.join(__dirname,'/views'))
@@ -19,7 +23,13 @@ app.get('/cats',(req,res)=>{
 })
 app.get('/r/:subreddit',(req,res)=>{
     const {subreddit}=req.params;
-    res.render('subreddit',{subreddit});
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit',{...data});
+    }else{
+        res.render('notfound',{subreddit})
+    }
+
 })
 app.listen(3000,()=>{
     console.log("LISTENING ON PORT 3000")
