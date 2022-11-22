f608d9c159a8c35c435777e47fbeca41137d6b79
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-11-22 10:03:29 +0530

    Parsing The Request Body Completed

--- Backend/ExpressJS/Defining RESTful Routes/Parsing The Request Body/index.js
+++ Backend/ExpressJS/Defining RESTful Routes/Parsing The Request Body/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app =express();
-app.use(express.urlencoded({extended:true}))
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
 app.get('/tacos',(req,res)=>{
     res.send("GET /tacos response")
     console.log("get")
 })
 app.post('/tacos',(req,res)=>{
-
-    res.send("POST /tacos request")
-    console.log("post");
+    const {meat, qty}=req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`)
 })
 app.listen(3000, ()=>{
     console.log("ON PORT 3000!")
