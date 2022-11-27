f7e80fd02b961fd5cdb73f502a011bf70903efcf
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2022-11-28 00:03:01 +0530

    RESTful Comments Overview Completed

--- Backend/ExpressJS/Defining RESTful Routes/RESTful Comments Overview/index.js
+++ Backend/ExpressJS/Defining RESTful Routes/RESTful Comments Overview/index.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const app =express();
+const path =require('path');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+const comments =[
+    {
+        username :'Todd',
+        comment:'lol that is so funny!'
+    },
+    {
+        username :'Skyler',
+        comment:'I like to go birdwatching with my dog'
+    },
+    {
+        username :'Sk8erBoi',
+        comment:'Plz delete your account, Todd'
+    },
+    {
+        username :'onlysayswoof',
+        comment:'woof woof woof'
+    },
+]
+
+app.get('/comments',
+(req,res)=>{
+res.render('comments/index',{comments})
+})
 
 app.get('/tacos',(req,res)=>{
     res.send("GET /tacos response")
     console.log("get")
 })
+
 app.post('/tacos',(req,res)=>{
     const {meat, qty}=req.body;
     res.send(`Ok, here are your ${qty} ${meat} tacos`)
 })
+
 app.listen(3000, ()=>{
     console.log("ON PORT 3000!")
 })
