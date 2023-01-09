f414e5166773f12c958856e7c80bd130e1e89f18
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-09 22:40:09 +0530

    Express Redirects Completed

--- Backend/ExpressJS/Defining RESTful Routes/RESTful Comments Overview/index.js
+++ Backend/ExpressJS/Defining RESTful Routes/RESTful Comments Overview/index.js
@@ -37,7 +37,7 @@ app.get('/comments/new',(req,res)=>{
 app.post('/comments',(req,res)=>{
     const{username,comment}=req.body;
     comments.push({username,comment})
-    res.send("IT WORKED!");
+    res.redirect('/comments')
 })
 
 app.get('/tacos',(req,res)=>{
