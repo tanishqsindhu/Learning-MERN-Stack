51627cdaf0a7400c419728e7350b143293232c39
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-11 13:56:55 +0530

    The UUID package Completed

--- Backend/ExpressJS/Defining RESTful Routes/RESTful Comments Overview/index.js
+++ Backend/ExpressJS/Defining RESTful Routes/RESTful Comments Overview/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app =express();
 const path =require('path');
+const { v4 : uuid} =require('uuid');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -8,22 +9,22 @@ app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 const comments =[
     {
-        id:1,
+        id: uuid(),
         username :'Todd',
         comment:'lol that is so funny!'
     },
     {
-        id:2,
+        id:uuid(),
         username :'Skyler',
         comment:'I like to go birdwatching with my dog'
     },
     {
-        id:3,
+        id:uuid(),
         username :'Sk8erBoi',
         comment:'Plz delete your account, Todd'
     },
     {
-        id:4,
+        id:uuid(),
         username :'onlysayswoof',
         comment:'woof woof woof'
     },
@@ -40,12 +41,12 @@ app.get('/comments/new',(req,res)=>{
 
 app.post('/comments',(req,res)=>{
     const{username,comment}=req.body;
-    comments.push({username,comment})
+    comments.push({username,comment, id:uuid()})
     res.redirect('/comments')
 })
 app.get('/comments/:id',(req,res)=>{
     const {id}=req.params;
-    const comment =comments.find(c=>c.id===parseInt(id));
+    const comment =comments.find(c=>c.id===id);
     res.render('comments/show',{comment});
 })
 app.get('/tacos',(req,res)=>{
