e5cdb942ea35c45f654dc09a34611fbb2d4d6150
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-14 23:29:12 +0530

    Finding with Mongoose Completed

--- Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/index.js
+++ Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/index.js
@@ -15,14 +15,14 @@ const movieSchema = new mongoose.Schema(
     });
 const Movie = mongoose.model('Movie',movieSchema);
 // const amadeus = new Movie({title:'Amadeus' ,year:1986,score:9.2,rating:'R'});
-Movie.insertMany([
-   {title:'Amelie',year:2001,score:8.3,rating:'R'}, 
-   {title:'Alien',year:1979,score:8.1,rating:'R'}, 
-   {title:'The Iron Giant',year:1999,score:7.5,rating:'PG'}, 
-   {title:'Stand By Me',year:1986,score:8.6,rating:'R'}, 
-   {title:'Moonrise Kingdom',year:2012,score:7.3,rating:'PG-13'}, 
-])
-.then(data=>{
-    console.log("IT WORKED!")
-    console.log(data);
-})
+// Movie.insertMany([
+//    {title:'Amelie',year:2001,score:8.3,rating:'R'}, 
+//    {title:'Alien',year:1979,score:8.1,rating:'R'}, 
+//    {title:'The Iron Giant',year:1999,score:7.5,rating:'PG'}, 
+//    {title:'Stand By Me',year:1986,score:8.6,rating:'R'}, 
+//    {title:'Moonrise Kingdom',year:2012,score:7.3,rating:'PG-13'}, 
+// ])
+// .then(data=>{
+//     console.log("IT WORKED!")
+//     console.log(data);
+// })
