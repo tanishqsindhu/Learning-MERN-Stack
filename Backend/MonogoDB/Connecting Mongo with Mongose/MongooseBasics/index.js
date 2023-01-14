73417716c2ece905528da985500e53c40e2681fc
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-14 20:47:10 +0530

    Our First Mongoose Model Completed

--- Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/index.js
+++ Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/index.js
@@ -6,3 +6,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieAppnode')
 .catch((err)=>{
     console.log(err)
 })
+const movieSchema = new mongoose.Schema(
+    {
+        Title: String,
+        Year: Number,
+        Score: Number,
+        Rating: String
+    });
+const Movie = mongoose.model('Movie',movieSchema);
+const amadeus = new Movie({Title:'Amadeus' ,Year:1986,Score:9.2,Rating:'R'});
