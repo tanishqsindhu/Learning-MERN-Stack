e2bb24ab829b863e38876e8ac062c1cea78d34b2
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-14 21:57:40 +0530

    Insert Many Completed

--- Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/index.js
+++ Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/index.js
@@ -8,10 +8,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/movieAppnode')
 })
 const movieSchema = new mongoose.Schema(
     {
-        Title: String,
-        Year: Number,
-        Score: Number,
-        Rating: String
+        title: String,
+        year: Number,
+        score: Number,
+        rating: String
     });
 const Movie = mongoose.model('Movie',movieSchema);
-const amadeus = new Movie({Title:'Amadeus' ,Year:1986,Score:9.2,Rating:'R'});
+// const amadeus = new Movie({title:'Amadeus' ,year:1986,score:9.2,rating:'R'});
+Movie.insertMany([
+   {title:'Amelie',year:2001,score:8.3,rating:'R'}, 
+   {title:'Alien',year:1979,score:8.1,rating:'R'}, 
+   {title:'The Iron Giant',year:1999,score:7.5,rating:'PG'}, 
+   {title:'Stand By Me',year:1986,score:8.6,rating:'R'}, 
+   {title:'Moonrise Kingdom',year:2012,score:7.3,rating:'PG-13'}, 
+])
+.then(data=>{
+    console.log("IT WORKED!")
+    console.log(data);
+})
