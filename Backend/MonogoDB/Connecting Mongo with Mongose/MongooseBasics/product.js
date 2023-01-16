ab131525a9f4c09079b62206244535fdb913e589
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-16 18:41:41 +0530

    Additional Schema Constraints Completed

--- Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/product.js
+++ Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/product.js
@@ -10,15 +10,32 @@ mongoose.connect('mongodb://127.0.0.1:27017/shopApp')
 const productSchema = new mongoose.Schema({
     name:{
         type:String,
-        required:true
+        required:true,
+        maxlength:20
     },
     price:{
         type:Number,
-        require:true
+        require:true,
+        min:0
+    },
+    onSale:{
+        type:Boolean,
+        default:false
+    },
+    categories:[String],
+    qty:{
+        online: {
+        type:Number,
+        default:0
+        },
+        inStore: {
+            type:Number,
+            default:0
+        }
     }
 })
 const Product = mongoose.model('Product',productSchema);
-const bike = new Product ({name:'Moutain Bike',price:599});
+const bike = new Product ({name:'Bike helmet',price:49.59,categories:['cycling','safety'],qty:{online:5,inStore:8}});
 bike.save()
 .then(data=>{
     console.log("IT WORKED!");
