96f38b069839ec07f9da19971d1542b4e6435ff1
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-16 21:27:44 +0530

    Adding Model Static Methods Completed

--- Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/product.js
+++ Backend/MonogoDB/Connecting Mongo with Mongose/MongooseBasics/product.js
@@ -69,6 +69,10 @@ productSchema.methods.addCategory = function (newCat){
     this.categories.push(newCat);
     return this.save();
 }
+
+productSchema.statics.fireSale=function(){
+    return this.updateMany({},{onSale:true,price:0})
+}
 const Product = mongoose.model('Product',productSchema);
 const findProduct = async ()=>{
     const foundProduct = await Product.findOne({name:'Mountain Bike'});
@@ -76,4 +80,5 @@ const findProduct = async ()=>{
     await foundProduct.toggleOnSale();
     console.log(foundProduct)
 }
-findProduct();
+Product.fireSale().then(res=> console.log(res));
+// findProduct();
