a620bc29a373307366bf3f7245e10053f299f24b
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-18 16:49:50 +0530

    Creating Our Model Completed

--- Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
+++ Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const Product =require('./models/product');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
-mongoose.connect('mongodb://127.0.0.1:27017/movieAppnode')
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(()=>{
-    console.log("CONNECTION SUCESSFULL")
+    console.log("MONGO CONNECTION OPEN!!!")
     })
 .catch((err)=>{
+    console.log("OH NO MONGO CONNECTION ERROR!!!!")
     console.log(err)
 })
 
