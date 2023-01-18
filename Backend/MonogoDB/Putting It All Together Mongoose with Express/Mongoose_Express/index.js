0eb73f321470939b9554b3ef0abe9d13c251a5bd
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-18 17:05:45 +0530

    Product Index Completed

--- Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
+++ Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
@@ -4,8 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Product =require('./models/product');
 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(()=>{
@@ -16,9 +15,16 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     console.log(err)
 })
 
-app.get('/dogs',(req,res)=>{
-res.send('WOOF!!');
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.get('/products',async(req,res)=>{
+    const products = await Product.find({})
+    console.log(products);
+    res.render('products/index',{products});
 })
+
+
 app.listen(3000,()=>{
     console.log("APP IS LISTENING ON port 3000")
 })
