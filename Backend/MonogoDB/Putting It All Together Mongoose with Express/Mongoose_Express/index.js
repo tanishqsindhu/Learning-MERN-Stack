bc83d9b36d64df00c82ba69332f44bf344d6405f
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-18 23:55:42 +0530

    Creating Products Completed

--- Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
+++ Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
@@ -4,8 +4,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Product =require('./models/product');
 
-
-
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(()=>{
     console.log("MONGO CONNECTION OPEN!!!")
@@ -17,17 +15,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}))
 
 app.get('/products',async(req,res)=>{
     const products = await Product.find({})
     console.log(products);
     res.render('products/index',{products});
 })
+app.get('/products/new',(req,res)=>{
+    res.render('products/new')
+})
+
+app.post('/products',async (req,res)=>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    // res.redirect('/products/show',{newProduct})
+    res.redirect(`/products/${newProduct._id}`)
+})
 
 app.get('/products/:id',async (req,res)=>{
     const {id} = req.params;
     const foundItem = await Product.findById(id);
-    console.log(foundItem);
     res.render('products/show',{foundItem})
 })
 app.listen(3000,()=>{
