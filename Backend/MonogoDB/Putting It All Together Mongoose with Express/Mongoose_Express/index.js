04d006472491dd836dbb9670df0cb95603fae7e9
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-19 00:39:51 +0530

    Updating Products Completed

--- Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
+++ Backend/MonogoDB/Putting It All Together Mongoose with Express/Mongoose_Express/index.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Product =require('./models/product');
+const methodOverride = require('method-override');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(()=>{
@@ -16,6 +18,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'));
 
 app.get('/products',async(req,res)=>{
     const products = await Product.find({})
@@ -38,6 +41,19 @@ app.get('/products/:id',async (req,res)=>{
     const foundItem = await Product.findById(id);
     res.render('products/show',{foundItem})
 })
+
+app.get('/products/:id/update',async (req,res)=>{
+    const {id} = req.params;
+    const foundItem = await Product.findById(id);
+    res.render('products/update',{foundItem})
+})
+
+app.put('/products/:id',async(req,res)=>{
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true})
+    res.redirect(`/products/${product._id}`)
+})
+
 app.listen(3000,()=>{
     console.log("APP IS LISTENING ON port 3000")
 })
