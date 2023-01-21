99ccd730defc19e1567fd72715161ad7ef34c0b8
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-22 00:10:06 +0530

    More Middleware Practice Completed

--- YelpCamp Project/Middleware The Key To Express/index.js
+++ YelpCamp Project/Middleware The Key To Express/index.js
@@ -5,23 +5,28 @@ const morgan = require('morgan');
 // morgan('tiny')
 
 app.use(morgan('dev'))
-
 app.use((req,res,next)=>{
-    console.log("THIS IS MY FIRST MIDDLEWARE!!!!")
-    return next();
-    console.log("THIS IS MY FIRST MIDDLEWARE!!!!  AFTER CALLING NEXT()")
-})
-
-app.use((req,res,next)=>{
-    console.log("THIS IS MY SECOND MIDDLEWARE!!!!")
+    req.requestTime = Date.now();
+    console.log(req.method.toUpperCase(),req.path);
     next();
 })
+// app.use((req,res,next)=>{
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!!")
+//     return next();
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!!  AFTER CALLING NEXT()")
+// })
+
+// app.use((req,res,next)=>{
+//     console.log("THIS IS MY SECOND MIDDLEWARE!!!!")
+//     next();
+// })
 
 app.get('/',(req,res)=>{
     res.send('HOME!')
 })
 
 app.get('/dogs',(req,res)=>{
+    console.log(`REQEUEST DATE: ${req.requestTime}`)
     res.send("WOOF WOOF!")
 })
 
