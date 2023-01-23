30d41c0544b782d1443e555a5aff10fdc21c1bd0
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-01-23 17:15:08 +0530

    A New EJS Tool For Layout Completed

--- YelpCamp Project/YelpCamp Adding Basic Styles/app.js
+++ YelpCamp Project/YelpCamp Adding Basic Styles/app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground =require('./models/campground');
 const methodOverride = require('method-override');
-
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(()=>{
@@ -16,8 +16,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 
 const app =express();
 
-
-
+app.engine('ejs',ejsMate);
 app.set('view engine','ejs');
 app.set ('views',path.join(__dirname,'views'));
 
