05022e50f546114d59d81b24bfc6a958da96a099
Tanishq Sindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-02-08 22:48:28 +0530

    Defining ExpresssError Class Completed

--- YelpCamp Project/YelpCamp Errors & Validating Data/app.js
+++ YelpCamp Project/YelpCamp Errors & Validating Data/app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const Campground =require('./models/campground');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const expressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catachAsync');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(()=>{
@@ -27,44 +29,42 @@ app.get('/',(req,res)=>{
     res.render('home')
 }); 
 
-app.get('/campgrounds',async(req,res)=>{
+app.get('/campgrounds',catchAsync(async(req,res)=>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index',{campgrounds})
-});
+}));
 
 app.get('/campgrounds/new',(req,res)=>{
     res.render('campgrounds/new');
 });
 
-app.get('/campgrounds/:id',async(req,res)=>{
+app.get('/campgrounds/:id',catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show',{campground});
-});
+}));
 
-app.get('/campgrounds/:id/edit',async(req,res)=>{
+app.get('/campgrounds/:id/edit',catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit',{campground});
-});
+}));
 
-app.post('/campgrounds',async(req,res,next)=>{
-    try{    const campground = new Campground(req.body.campground);
+app.post('/campgrounds',catchAsync(async(req,res,next)=>{
+const campground = new Campground(req.body.campground);
         await campground.save();
-        res.redirect(`/campgrounds/${campground._id}`);}
-        catch(e){
-            next(e);
-    }
-});
+        res.redirect(`/campgrounds/${campground._id}`);
+    }));
 
-app.put('/campgrounds/:id',async(req,res)=>{
+app.put('/campgrounds/:id',catchAsync(async(req,res)=>{
     const {id} = req.params ;
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground},{runValidators:true,new:true})
     res.redirect(`/campgrounds/${campground._id}`);
-})
-app.delete('/campgrounds/:id',async(req,res)=>{
+}));
+
+app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     const {id} = req.params;
     await Campground.findByIdAndDelete(id)
     res.redirect(`/campgrounds/`);
-})
+}));
 
 app.use((err,req,res,next)=>{
 res.send('Oh Boy, Something went wrong!')
