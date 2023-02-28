b56ae35805a309b2b5d4b8f5dbb96c36fc20e733
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-02-28 17:37:30 +0530

    Defining The Review Model Completed

--- YelpCamp Project/YelpCamp Adding The Review Model/models/campground.js
+++ YelpCamp Project/YelpCamp Adding The Review Model/models/campground.js
@@ -6,7 +6,11 @@ const CampgroundSchema = new Schema ({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews:[{
+        types:Schema.Types.ObjectId,
+        ref:'Review'
+    }]
 });
 
 module.exports= mongoose.model('Campground', CampgroundSchema);
