b46ffc95d3eb00d276505baf8cc920a81d61de20
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-07 15:18:55 +0530

    More Express Session Completed

--- Backend/ExpressJS/Express Session and Flash/session-demo/index.js
+++ Backend/ExpressJS/Express Session and Flash/session-demo/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 
-app.use(session({secret:'thisisnotgoodseceret'}))
+const sessionOptions = {secret:'thisisnotgoodseceret',resave:false,saveUninitialized:false}
+app.use(session(sessionOptions))
 
 app.get('/viewcount',(req,res)=>{
     if(req.session.count){
@@ -11,6 +12,17 @@ app.get('/viewcount',(req,res)=>{
         req.session.count=1;
     }
     res.send(`YOU HAVE VIEWED THIS PAGE ${req.session.count} TIMES`)
+})
+
+app.get('/register',(req,res)=>{
+    const {username = 'Anonymous'}=req.query;
+    req.session.username = username;
+    res.send(username);
+})
+
+app.get('/greet',(req,res)=>{
+    const {username} = req.session;
+    res.send(`Hello! ${username}`)
 })
 
 app.listen(3000,()=>{
