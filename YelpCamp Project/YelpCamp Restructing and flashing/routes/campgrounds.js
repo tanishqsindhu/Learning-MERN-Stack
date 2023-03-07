c9aeb7821878738a5c5b6e13a64dbc696b3c3abe
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-07 18:41:40 +0530

    Flash Errors Partial Completed

--- YelpCamp Project/YelpCamp Restructing and flashing/routes/campgrounds.js
+++ YelpCamp Project/YelpCamp Restructing and flashing/routes/campgrounds.js
@@ -36,11 +36,19 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+        req.flash('error','CANNOT FIND THE CAMPGROUND')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', { campground});
 }));
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if(!campground){
+        req.flash('error','CANNOT FIND THE CAMPGROUND')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/edit', { campground });
 }))
 
