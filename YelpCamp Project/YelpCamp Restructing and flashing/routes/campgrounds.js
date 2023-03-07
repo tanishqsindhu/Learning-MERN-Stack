a046902369ec42a41ddea957f394620d8120718b
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-07 18:08:58 +0530

    Flash Success Partial Completed

--- YelpCamp Project/YelpCamp Restructing and flashing/routes/campgrounds.js
+++ YelpCamp Project/YelpCamp Restructing and flashing/routes/campgrounds.js
@@ -30,7 +30,7 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
-    req.flash('success','Successfully made a new homeground!')
+    req.flash('success','Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
@@ -47,6 +47,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success','Successfully updated the campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
