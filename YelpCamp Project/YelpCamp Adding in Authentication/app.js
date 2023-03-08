25feba6caa53f360b7666dbb10c17212d0478a45
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-08 21:39:44 +0530

    Configuring Passport Completed

--- YelpCamp Project/YelpCamp Adding in Authentication/app.js
+++ YelpCamp Project/YelpCamp Adding in Authentication/app.js
@@ -6,8 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-
-
+const passport =require('passport');
+const LocalStrategy=require('passport-local')
+const User = require('./models/user')
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 
@@ -43,10 +44,23 @@ const sessionConfig = {
 app.use(session(sessionConfig))
 app.use(flash());
 
+app.use(passport.initialize);
+app.use(passport.session);
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
+})
+
+app.get('/fakeUser',async(req,res)=>{
+    const user=new User({email:'cult@gmail.com',username:'cultcurt'});
+    const newUser = await User.register(user,'cult');
+    res.send(newUser);
 })
 
 app.use('/campgrounds', campgrounds)
--- YelpCamp Project/YelpCamp Adding in Authentication/models/user.js
+++ YelpCamp Project/YelpCamp Adding in Authentication/models/user.js
@@ -12,4 +12,4 @@ const UserSchema =new Schema({
 })
 UserSchema.plugin(passportLocalMongoose);
 
-model.exports=mongoose.model('User',UserSchema);
+module.exports=mongoose.model('User',UserSchema);
