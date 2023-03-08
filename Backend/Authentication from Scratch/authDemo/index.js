b87d38214156da95a5a9a0c0a13d77a9b81fa981
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-08 19:48:45 +0530

    Auth Demo: Refactoring To Model Methods Completed

--- Backend/Authentication from Scratch/authDemo/index.js
+++ Backend/Authentication from Scratch/authDemo/index.js
@@ -34,14 +34,10 @@ app.get('/register',(req,res)=>{
 })
 app.post('/register',async(req,res)=>{
     const {password,username}=req.body;
-    const hash = await bcrypt.hash(password,12);
-    const user=new User({
-        username,
-        password:hash
-    })
+    const user=new User({username,password})
     await user.save()
     req.session.user_id = user._id;
-    res.redirect('/')
+    res.redirect('/secret')
 })
 
 app.get('/login',(req,res)=>{
@@ -50,10 +46,9 @@ app.get('/login',(req,res)=>{
 
 app.post('/login',async(req,res)=>{
     const {username,password}=req.body;
-    const user = await User.findOne({username});
-    const validPassword = await bcrypt.compare(password,user.password);
-    if(validPassword){
-        req.session.user_id = user._id;
+    const foundUser = await User.findAndValidate(username,password);
+    if(foundUser){
+        req.session.user_id = foundUser._id;
         res.redirect('/secret')
     }else{
         res.redirect('/login')
