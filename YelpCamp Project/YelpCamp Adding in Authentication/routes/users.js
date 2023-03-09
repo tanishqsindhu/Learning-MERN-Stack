fe1ff1a99a770ee51a070f89692c16bdd37b361c
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-09 18:34:00 +0530

    Fixing Register Route Completed

--- YelpCamp Project/YelpCamp Adding in Authentication/routes/users.js
+++ YelpCamp Project/YelpCamp Adding in Authentication/routes/users.js
@@ -8,13 +8,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register',catchAsync(async(req,res)=>{
+router.post('/register',catchAsync(async(req,res,next)=>{
     try{
         const {email,username,password}= req.body
         const user = await new User({email,username});
         const registeredUser = await User.register(user,password);
-        req.flash('success','Welcome to Yelp-Camp');
-        res.redirect('/campgrounds');
+        req.login(registeredUser,err=>{
+            if(err) return next(err);
+            req.flash('success','Welcome to Yelp-Camp');
+            res.redirect('/campgrounds');
+        });
     } catch(e){
         req.flash('error',e.message);
         res.redirect('/register');
