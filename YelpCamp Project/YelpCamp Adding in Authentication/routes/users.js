b7a9bb6781e6747f0bfff3c5a67523422ba57bab
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-09 18:09:31 +0530

    Adding Logout Completed

--- YelpCamp Project/YelpCamp Adding in Authentication/routes/users.js
+++ YelpCamp Project/YelpCamp Adding in Authentication/routes/users.js
@@ -31,4 +31,12 @@ router.post('/login',passport.authenticate('local',{failureFlash:true,failureRed
     res.redirect('/campgrounds')
 })
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      req.flash('success', "Goodbye!");
+      res.redirect('/campgrounds');
+    });
+  }); 
+
 module.exports=router;
