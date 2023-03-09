08ec58919c6aaf3b80710847973d17da684b7fec
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-09 18:56:15 +0530

    YelpCamp Basic Authorization Section Started

---
+++ YelpCamp Project/YelpCamp Basic Authorization/app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const flash = require('connect-flash');
+const ExpressError = require('./utils/ExpressError');
+const methodOverride = require('method-override');
+const passport =require('passport');
+const LocalStrategy=require('passport-local')
+const User = require('./models/user')
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
+const usersRoutes = require('./routes/users')
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(()=>{
+    console.log("MONGO CONNECTION OPEN!!!")
+    })
+.catch((err)=>{
+    console.log("OH NO MONGO CONNECTION ERROR!!!!")
+    console.log(err)
+})
+
+const app = express();
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+    res.locals.currentUser=req.user;
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+app.get('/fakeUser',async(req,res)=>{
+    const user=new User({email:'cult@gmail.com',username:'cultcurt'});
+    const newUser = await User.register(user,'cult');
+    res.send(newUser);
+})
+
+app.use('/campgrounds', campgroundsRoutes)
+app.use('/campgrounds/:id/reviews', reviewsRoutes)
+app.use('/', usersRoutes)
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
+
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
