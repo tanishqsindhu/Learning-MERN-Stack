17729e37279cb4b37c1a0ef6166208b600e9e8fd
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-10 21:10:38 +0530

    A Fancy Way To Restructure Routes Completed

--- YelpCamp Project/YelpCamp Controllers and Star Ratings/routes/campgrounds.js
+++ YelpCamp Project/YelpCamp Controllers and Star Ratings/routes/campgrounds.js
@@ -4,18 +4,17 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgrounds=require('../controllers/campgrounds');
 
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get( catchAsync(campgrounds.index))
+    .post( isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put( isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete( isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
