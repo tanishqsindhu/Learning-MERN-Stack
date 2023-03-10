f22b73df34e9047101166afa2c2c1bf0f40e87c0
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-10 23:54:11 +0530

    Fixing Our Seeds Completed

--- YelpCamp Project/YelpCamp Image Upload/seeds/index.js
+++ YelpCamp Project/YelpCamp Image Upload/seeds/index.js
@@ -31,7 +31,20 @@ const seedDB = async () => {
             author:'6409df6bd7ead54475fd3e28',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dqwbbhysb/image/upload/v1678472436/YelpCamp/r4ausnpaqlaxn4gajrdj.png',
+                  filename: 'YelpCamp/r4ausnpaqlaxn4gajrdj',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dqwbbhysb/image/upload/v1678472437/YelpCamp/wrm5ixup1ocwloft9u8c.jpg',
+                  filename: 'YelpCamp/wrm5ixup1ocwloft9u8c',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dqwbbhysb/image/upload/v1678472438/YelpCamp/r9qhvbizvdkq5itt2lxe.jpg',
+                  filename: 'YelpCamp/r9qhvbizvdkq5itt2lxe',
+                }
+              ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price
         })
