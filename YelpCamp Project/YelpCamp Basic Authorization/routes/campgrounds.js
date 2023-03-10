17c244a21cf81cd8907ef30650c18036fe5df8a8
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-10 17:31:10 +0530

    Fixed console.log

--- YelpCamp Project/YelpCamp Basic Authorization/routes/campgrounds.js
+++ YelpCamp Project/YelpCamp Basic Authorization/routes/campgrounds.js
@@ -30,7 +30,6 @@ router.get('/:id', catchAsync(async (req, res,) => {
             path: 'author'
         }
     }).populate('author');
-    console.log(campground);
     if (!campground) {
         req.flash('error', 'Cannot find that campground!');
         return res.redirect('/campgrounds');
