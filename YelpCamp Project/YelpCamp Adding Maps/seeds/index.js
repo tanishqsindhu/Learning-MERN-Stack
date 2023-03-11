d7ed1235e2ba7b7ba0052a7375a1abe593c8a68c
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-12 00:04:56 +0530

    Fixing Our Seeds Bug Completed

--- YelpCamp Project/YelpCamp Adding Maps/seeds/index.js
+++ YelpCamp Project/YelpCamp Adding Maps/seeds/index.js
@@ -29,6 +29,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author:'6409df6bd7ead54475fd3e28',
+            geometry: { type: 'Point', coordinates: [ -122.330062, 47.603832 ] },
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             images: [
