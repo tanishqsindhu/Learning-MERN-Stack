6a7191982e3e4845d4696f1a7c02748c68dfd261
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-12 00:28:11 +0530

    YelpCamp Fancy Cluster Map Initialize

---
+++ YelpCamp Project/YelpCamp Fancy Cluster Map/models/campground.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema({
+        url:String,
+        filename:String
+});
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload','/upload/w_200')
+});
+
+const CampgroundSchema = new Schema({
+    title: String,
+    images:[ImageSchema],
+    geometry: {
+        type: {
+          type: String, 
+          enum: ['Point'],
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+      },
+    price: Number,
+    description: String,
+    location: String,
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
---
+++ YelpCamp Project/YelpCamp Fancy Cluster Map/seeds/index.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(()=>{
+    console.log("MONGO CONNECTION OPEN!!!")
+    })
+.catch((err)=>{
+    console.log("OH NO MONGO CONNECTION ERROR!!!!")
+    console.log(err)
+})
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            author:'6409df6bd7ead54475fd3e28',
+            geometry: { type: 'Point', coordinates: [ -122.330062, 47.603832 ] },
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dqwbbhysb/image/upload/v1678472436/YelpCamp/r4ausnpaqlaxn4gajrdj.png',
+                  filename: 'YelpCamp/r4ausnpaqlaxn4gajrdj',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dqwbbhysb/image/upload/v1678472437/YelpCamp/wrm5ixup1ocwloft9u8c.jpg',
+                  filename: 'YelpCamp/wrm5ixup1ocwloft9u8c',
+                },
+                {
+                  url: 'https://res.cloudinary.com/dqwbbhysb/image/upload/v1678472438/YelpCamp/r9qhvbizvdkq5itt2lxe.jpg',
+                  filename: 'YelpCamp/r9qhvbizvdkq5itt2lxe',
+                }
+              ],
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
