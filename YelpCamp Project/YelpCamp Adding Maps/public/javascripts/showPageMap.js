9228be80c1b85ca723a6214ec64f289f517ae62a
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-11 23:53:59 +0530

    Centering The Map On A Campground Completed

--- YelpCamp Project/YelpCamp Adding Maps/public/javascripts/showPageMap.js
+++ YelpCamp Project/YelpCamp Adding Maps/public/javascripts/showPageMap.js
@@ -2,6 +2,9 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 9, // starting zoom
 });
+const marker2 = new mapboxgl.Marker({ color: 'black' })
+.setLngLat(campground.geometry.coordinates)
+.addTo(map);
