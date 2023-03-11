cc9b3b62b3bd0f6ecc75a7cc7875250ba772fff2
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-11 16:24:00 +0530

    Geocoding Our Locations Completed

--- YelpCamp Project/YelpCamp Adding Maps/controllers/campgrounds.js
+++ YelpCamp Project/YelpCamp Adding Maps/controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
-const{cloudinary} = require('../cloudinary')
+const{cloudinary} = require('../cloudinary');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const MapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken:MapBoxToken});
 
 module.exports.index=async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -11,12 +14,17 @@ module.exports.renderNewForm=(req, res) => {
 }
 
 module.exports.createCampground=async (req, res, next) => {
-    const campground = new Campground(req.body.campground);
-    campground.images= req.files.map(f=>({url:f.path,filename:f.filename}));
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfully made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`)
+    const geoData = await geocoder.forwardGeocode({
+        query:req.body.campground.location,
+        limit:1
+    }).send()
+    res.send(geoData.body.features[0].geometry.coordinates)
+    // const campground = new Campground(req.body.campground);
+    // campground.images= req.files.map(f=>({url:f.path,filename:f.filename}));
+    // campground.author = req.user._id;
+    // await campground.save();
+    // req.flash('success', 'Successfully made a new campground!');
+    // res.redirect(`/campgrounds/${campground._id}`)
 }
 
 module.exports.showCampground=async (req, res,) => {
