d35a65c45759c42c2c66d8ab75bc6859b7aed8d0
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-14 16:17:56 +0530

    YelpCamp Deploying Section Initialize

---
+++ YelpCamp Project/YelpCamp Deploying/routes/reviews.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const catchAsync = require('../utils/catchAsync');
+const reviews=require('../controllers/reviews')
+
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+
+module.exports = router;
