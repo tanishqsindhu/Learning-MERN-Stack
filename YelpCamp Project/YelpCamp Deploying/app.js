77369e78e6acd78b7d3189e2da45383242d9af47
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-14 17:51:26 +0530

    Fixing Heroku Errors Completed

--- YelpCamp Project/YelpCamp Deploying/app.js
+++ YelpCamp Project/YelpCamp Deploying/app.js
@@ -153,7 +153,8 @@ app.use((err, req, res, next) => {
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
+const port=process.env.PORT||"3000"
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
