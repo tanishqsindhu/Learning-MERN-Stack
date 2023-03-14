04fb0c3c8d3d858bef2efe83962cc8abab4832c8
tanishqsindhu <85939793+tanishqsindhu@users.noreply.github.com>
2023-03-14 13:54:41 +0530

    Removing Inline Map Styles Completed

--- YelpCamp Project/YelpCamp Styles Clean Up/public/javascripts/clusterMap.js
+++ YelpCamp Project/YelpCamp Styles Clean Up/public/javascripts/clusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
